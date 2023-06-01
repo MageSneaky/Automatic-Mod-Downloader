edff9ccd7be27b120ac6ca6e224984c8160982ca
Alexander <alex.hjortsberg@gmail.com>
2023-06-01 22:59:30 +0200

    ModList, Mod Downloading, UI Improvements

--- Main.Designer.cs
+++ Main.Designer.cs
@@ -31,6 +31,9 @@
             System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Main));
             this.listBox1 = new System.Windows.Forms.ListBox();
             this.SettingsButton = new System.Windows.Forms.Button();
+            this.downloadSelected = new System.Windows.Forms.RadioButton();
+            this.downloadAll = new System.Windows.Forms.RadioButton();
+            this.downloadButton = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
             // listBox1
@@ -38,10 +41,16 @@
             this.listBox1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
             | System.Windows.Forms.AnchorStyles.Left) 
             | System.Windows.Forms.AnchorStyles.Right)));
+            this.listBox1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(60)))), ((int)(((byte)(60)))), ((int)(((byte)(60)))));
+            this.listBox1.Font = new System.Drawing.Font("Inter", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.listBox1.ForeColor = System.Drawing.Color.White;
             this.listBox1.FormattingEnabled = true;
+            this.listBox1.ItemHeight = 16;
             this.listBox1.Location = new System.Drawing.Point(12, 38);
             this.listBox1.Name = "listBox1";
-            this.listBox1.Size = new System.Drawing.Size(776, 381);
+            this.listBox1.SelectionMode = System.Windows.Forms.SelectionMode.MultiExtended;
+            this.listBox1.Size = new System.Drawing.Size(776, 372);
+            this.listBox1.Sorted = true;
             this.listBox1.TabIndex = 0;
             // 
             // SettingsButton
@@ -52,11 +61,46 @@
             this.SettingsButton.FlatAppearance.BorderSize = 0;
             this.SettingsButton.FlatAppearance.MouseOverBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(80)))), ((int)(((byte)(80)))), ((int)(((byte)(80)))));
             this.SettingsButton.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
-            this.SettingsButton.Location = new System.Drawing.Point(759, 421);
+            this.SettingsButton.Location = new System.Drawing.Point(760, 416);
             this.SettingsButton.Name = "SettingsButton";
-            this.SettingsButton.Size = new System.Drawing.Size(26, 26);
+            this.SettingsButton.Size = new System.Drawing.Size(28, 28);
             this.SettingsButton.TabIndex = 1;
             this.SettingsButton.UseVisualStyleBackColor = true;
+            // 
+            // downloadSelected
+            // 
+            this.downloadSelected.AutoSize = true;
+            this.downloadSelected.ForeColor = System.Drawing.Color.White;
+            this.downloadSelected.Location = new System.Drawing.Point(105, 12);
+            this.downloadSelected.Name = "downloadSelected";
+            this.downloadSelected.Size = new System.Drawing.Size(118, 17);
+            this.downloadSelected.TabIndex = 2;
+            this.downloadSelected.Text = "Download Selected";
+            this.downloadSelected.UseVisualStyleBackColor = true;
+            // 
+            // downloadAll
+            // 
+            this.downloadAll.AutoSize = true;
+            this.downloadAll.Checked = true;
+            this.downloadAll.ForeColor = System.Drawing.Color.White;
+            this.downloadAll.Location = new System.Drawing.Point(12, 12);
+            this.downloadAll.Name = "downloadAll";
+            this.downloadAll.Size = new System.Drawing.Size(87, 17);
+            this.downloadAll.TabIndex = 3;
+            this.downloadAll.TabStop = true;
+            this.downloadAll.Text = "Download All";
+            this.downloadAll.UseVisualStyleBackColor = true;
+            // 
+            // downloadButton
+            // 
+            this.downloadButton.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+            this.downloadButton.Location = new System.Drawing.Point(12, 416);
+            this.downloadButton.Name = "downloadButton";
+            this.downloadButton.Size = new System.Drawing.Size(129, 28);
+            this.downloadButton.TabIndex = 4;
+            this.downloadButton.Text = "Download";
+            this.downloadButton.UseVisualStyleBackColor = true;
+            this.downloadButton.Click += new System.EventHandler(this.downloadButton_Click);
             // 
             // Main
             // 
@@ -64,11 +108,16 @@
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(40)))), ((int)(((byte)(40)))), ((int)(((byte)(40)))));
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.downloadButton);
+            this.Controls.Add(this.downloadAll);
+            this.Controls.Add(this.downloadSelected);
             this.Controls.Add(this.SettingsButton);
             this.Controls.Add(this.listBox1);
             this.Name = "Main";
             this.Text = "Automatic Mod Downloader";
+            this.Load += new System.EventHandler(this.Main_Load);
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
@@ -76,6 +125,9 @@
 
         private System.Windows.Forms.ListBox listBox1;
         private System.Windows.Forms.Button SettingsButton;
+        private System.Windows.Forms.RadioButton downloadSelected;
+        private System.Windows.Forms.RadioButton downloadAll;
+        private System.Windows.Forms.Button downloadButton;
     }
 }
 
--- Main.cs
+++ Main.cs
@@ -1,21 +1,205 @@
-﻿using System;
+﻿using Newtonsoft.Json;
+using System;
 using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
 using System.Drawing;
+using System.Drawing.Text;
+using System.IO;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.Net;
+using System.Security.Principal;
 using System.Windows.Forms;
 
 namespace Minecraft_Automatic_ModDownloader
 {
     public partial class Main : Form
     {
+        private string modsLink = "";
+        private List<Mod> modList = new List<Mod>();
+
+        private string message = "";
+        private DateTime msgTime;
+
+        private string userName = Environment.UserName;
+
+        private string logDir = Directory.GetCurrentDirectory() + @"\log.txt";
+        private string minecraftDir = "C:/Users/" + Environment.UserName + "/AppData/Roaming/.minecraft";
+
         public Main()
         {
             InitializeComponent();
+            var configfile = new IniFile("config.ini");
+            string configDir = Directory.GetCurrentDirectory() + @"\config.ini";
 
+            if (!File.Exists("config.ini"))
+            {
+                configfile.Write("ModsJson", modsLink);
+            }
+            else
+            {
+                modsLink = configfile.Read("ModsJson");
+            }
+
+            if (!Directory.Exists(Directory.GetCurrentDirectory() + @"\mods"))
+            {
+                Directory.CreateDirectory(Directory.GetCurrentDirectory() + @"\mods");
+            }
+
+            if (modsLink == "")
+            {
+                DialogResult result;
+                result = MessageBox.Show(
+                    "No mods link found in config in\n" + configDir,
+                    "No mods link found in config",
+                    MessageBoxButtons.OK,
+                    MessageBoxIcon.Warning
+                );
+                message = "No mods link found in config in" + configDir;
+                LogMsg();
+            }
+            else
+            {
+                if (!modsLink.EndsWith(".json"))
+                {
+                    DialogResult result;
+                    result = MessageBox.Show(
+                        "Invalid mods link found in config in\n" + configDir + "\nDoes not end with .json",
+                        "Invalid mods link found in config",
+                        MessageBoxButtons.OK,
+                        MessageBoxIcon.Warning
+                    );
+                    message = "Invalid mods link found in config in " + configDir + "\nDoes not end with .json";
+                    LogMsg();
+                }
+            }
+
+            if(!IsAdministrator())
+            {
+                DialogResult result;
+                result = MessageBox.Show(
+                    "Not running application as administrator can not download into minecraft folder installing into mods folder instead",
+                    "Not running application as administrator",
+                    MessageBoxButtons.OK,
+                    MessageBoxIcon.Warning
+                );
+                message = "Invalid mods link found in config in " + configDir + "\nDoes not end with .json";
+                LogMsg();
+            }
+
+            if (!Directory.Exists(minecraftDir + @"\mods"))
+            {
+                DialogResult result;
+                result = MessageBox.Show(
+                    "No mods folder found at " + minecraftDir + "\tDownload forge here: https://files.minecraftforge.net/net/minecraftforge/forge/",
+                    "No mods folder found",
+                    MessageBoxButtons.OK,
+                    MessageBoxIcon.Error
+                );
+                message = "No mods folder found at C:/Users/" + userName + "/AppData/Roaming/.minecraft\tDownload forge here: https://files.minecraftforge.net/net/minecraftforge/forge/";
+                LogMsg();
+                Application.Exit();
+                this.Close();
+            }
+            else
+            {
+                if (CheckLink(modsLink))
+                {
+                    GetMods(modsLink);
+                }
+            }
+        }
+
+        private void LogMsg()
+        {
+            msgTime = DateTime.Now;
+            StreamWriter sw = new StreamWriter(logDir, true);
+            sw.WriteLine($"[{msgTime}] {message}");
+            sw.Close();
+        }
+
+        private void Main_Load(object sender, EventArgs e)
+        {
+            PrivateFontCollection pfc = new PrivateFontCollection();
+            pfc.AddFontFile(@"C:\Users\alleh\OneDrive\Dokument\!C#_Projects\Minecraft_Automatic_ModDownloader\Assets\Inter-Regular.ttf");
+            foreach (Control c in this.Controls)
+            {
+                c.Font = new Font(pfc.Families[0], 10, FontStyle.Regular);
+            }
+        }
+
+        private void downloadButton_Click(object sender, EventArgs e)
+        {
+            string downloadFolder = minecraftDir + @"\mods";
+            if(!IsAdministrator())
+            {
+                downloadFolder = Directory.GetCurrentDirectory() + @"\mods";
+            }
+            if (CheckLink(modsLink))
+            {
+                using (WebClient wc = new WebClient())
+                {
+                    foreach (var mod in modList)
+                    {
+                        wc.DownloadFile(mod.Download.ToString(), downloadFolder);
+                    }
+                }
+            }
+        }
+
+        private void GetMods(string modLink)
+        {
+            using (WebClient wc = new WebClient())
+            {
+                var webjson = wc.DownloadString(modLink.ToString());
+                dynamic json = JsonConvert.DeserializeObject(webjson);
+                modList.Clear();
+                List<string> list = new List<string>();
+                foreach (var mod in json)
+                {
+                    message = "Name: " + mod.name + " | Download: " + mod.download;
+                    LogMsg();
+                    Mod newMod = new Mod();
+                    newMod.Name = mod.name;
+                    newMod.Download = mod.download;
+                    modList.Add(newMod);
+                    list.Add(mod.name.ToString());
+                }
+                listBox1.DataSource = list;
+            }
+        }
+
+        private bool CheckLink(string link)
+        {
+            if (link != "")
+            {
+                if (link.EndsWith(".json"))
+                {
+                    return true;
+                }
+            }
+            return false;
+        }
+
+        public static bool IsAdministrator()
+        {
+            var identity = WindowsIdentity.GetCurrent();
+            var principal = new WindowsPrincipal(identity);
+            return principal.IsInRole(WindowsBuiltInRole.Administrator);
+        }
+    }
+
+    class Mod
+    {
+        private string name;
+        public string Name
+        {
+            get { return name; }
+            set { name = value; }
+        }
+        private string download;
+        public string Download
+        {
+            get { return download; }
+            set { download = value; }
         }
     }
 }
